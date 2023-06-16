ef2684ce79142e27151cdb5dac484c52d087e3b6
Oleg Kremliakov <olegdnepr@gmail.com>
2023-06-16 23:39:50 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,103 @@
+//import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client"
+import "./index.css"
+
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+
+  
+
+function App(){
+
+    return <div>
+        <Header />  
+        <Menu />
+        <Footer />
+        </div>
+
+}
+
+function Pizza(){
+
+    return <div>
+        
+            <h2>Pizza Spinacci</h2>
+            <img src="/pizzas/spinaci.jpg"></img>
+        
+    </div>
+}
+
+
+function Menu (){
+
+    const hour = new Date().getHours();
+    const openHour = 9
+    const closeHour = 22
+    const isOpen = openHour <= hour && closeHour>= hour;
+
+    return <div><h2>Our Menu</h2>
+
+    <Pizza />
+    <Pizza />
+    <Pizza />
+    </div>
+}
+
+function Header(){
+
+   return  <header className="header">
+            <h1>Welcome to Pizza React!</h1>
+
+    </header>
+   
+}
+
+function Footer(){
+
+    return <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+}
+const root =  ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
